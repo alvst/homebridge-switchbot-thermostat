c55fcd5652709126205299eceb6d8cdbfbc8377b
Alvie Stoddard <alviestoddard@gmail.com>
2022-12-27 17:44:39 -0500

    .

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function Thermostat(log, config) {
   console.log(config.thermostat_configuration);
   // console.log(config.thermostat_configuration['bearerToken']);
 
-  console.log(config['thermostat_configuration'].bearerToken);
+  console.log(config['thermostat_configuration']);
 
   this.bearerToken = config.thermostat_configuration['bearerToken'];
   this.power_switch_accessory_uuid =

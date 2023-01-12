5f9ed5709516cfefb4beaeed4eddb55dcbeb3015
Alvie Stoddard <alviestoddard@gmail.com>
2023-01-11 22:03:59 -0500

    .

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ module.exports = function (homebridge) {
   Service = homebridge.hap.Service;
   Characteristic = homebridge.hap.Characteristic;
   homebridge.registerAccessory(
-    'homebridge-web-thermostat2',
+    'homebridge-web-thermostat',
     'Thermostat',
     Thermostat
   );
